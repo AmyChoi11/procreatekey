4c58cc06807dde376795f405e8a1e43c7e2d0997
Library-LBE015 <library-lbe015@lbe015.ust.hk>
2025-11-19 14:05:02 +0800

    Add imageScale modifier to bluetooth icon for better visibility

--- ProcreateBLEConfig/ContentView.swift
+++ ProcreateBLEConfig/ContentView.swift
@@ -435,6 +435,7 @@ struct ContentView: View {
                                 Image(systemName: "bluetooth")
                                     .font(.system(size: 16, weight: .medium))
                                     .foregroundColor(bleManager.isConnected ? Color.green : Color(red: 0.4, green: 0.2, blue: 0.6))
+                                    .imageScale(.medium)
                                 Text(bleManager.isConnected ? "Connected" : "Scan")
                                     .font(.system(size: 15, weight: .semibold))
                                     .foregroundColor(bleManager.isConnected ? Color.green : Color(red: 0.4, green: 0.2, blue: 0.6))
